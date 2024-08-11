dd47c02104d32b8fed38829988b487c3118a7732
gautamchauhan0512 <chauhangdc756@gmail.com>
2024-08-11 10:34:56 +0530

    Pass First TestCase:Implement add book feature

--- src/Library.java
+++ src/Library.java
@@ -1,4 +1,32 @@
+import java.util.*;
+import java.util.ArrayList;
+
 public class Library {
 
-    
+    String isbn;
+    String title;
+    String author;
+    int year;
+    boolean isBorrowed; // Flag to indicate if the book is borrowed
+    private static ArrayList<Library> library = new ArrayList<>();
+
+    public Library(String isbn, String title, String author, int year) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.isBorrowed = false; // Initially, the book is not borrowed
+    }
+
+    public void addBook(String isbn, String title, String author, int year) {
+
+        Library newBook = new Library(isbn, title, author, year);
+        library.add(newBook);
+        System.out.println("Book is added");
+    }
+
+    public int lengthOfLibrary() {
+        return library.size();
+    }
+
 }
--- src/LibraryTest.java
+++ src/LibraryTest.java
@@ -1,10 +1,15 @@
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.*;;
 public class LibraryTest {
-@Test
-public void addbookTest(){
-    Library addbook = new Library();
-    addbook.addbook();
-}
-    
+ 
+    Library obj = new Library("","","",0);
+    @Test
+    public void TestaddBook()
+    {
+        obj.addBook("isbn111", "my name", "gautam chauhan", 2024);
+        int result = obj.lengthOfLibrary();
+        assertEquals(1, result,"Book is added");
+    }  
 }
