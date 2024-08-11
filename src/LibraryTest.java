8808bb076c7376ba25c499484c85d5482eb2be88
gautamchauhan0512 <chauhangdc756@gmail.com>
2024-08-11 17:05:00 +0530

    Refactor : change the book name

--- src/LibraryTest.java
+++ src/LibraryTest.java
@@ -9,50 +9,50 @@ public class LibraryTest {
 
     @Test
     public void testAddBook() {
-        obj.addBook("isbn111", "my name", "gautam chauhan", 2024);
+        obj.addBook("isbn111", "my village", "gautam chauhan", 2024);
         int result = obj.lengthOfLibrary();
         assertEquals(1, result, "Book is added");
     }
 
     @Test
     public void testAddDuplicateBook() {
-        obj.addBook("isbn111", "my name", "gautam chauhan", 2024);
-        obj.addBook("isbn111", "my name", "gautam chauhan", 2024);
+        obj.addBook("isbn111", "my village", "gautam chauhan", 2024);
+        obj.addBook("isbn111", "my village", "gautam chauhan", 2024);
         int result = obj.lengthOfLibrary();
         assertEquals(1, result, "Duplicate book should not be added.");
     }
 
     @Test
     public void testBorrowBook() {
-        obj.addBook("isbn111", "my name", "gautam chauhan", 2024);
-        obj.borrowBook("my name");
+        obj.addBook("isbn111", "my village", "gautam chauhan", 2024);
+        obj.borrowBook("my village");
         int result = obj.availableBook();
         assertEquals(0, result, "You have borrow the book ");
     }
 
     @Test
     public void testAlreadBorrowOrUnavailable() {
-        obj.addBook("isbn111", "my name", "gautam chauhan", 2024);
-        obj.borrowBook("my name");
-        obj.borrowBook("my name");
-        obj.borrowBook("My namee");
+        obj.addBook("isbn111", "my village", "gautam chauhan", 2024);
+        obj.borrowBook("my village");
+        obj.borrowBook("my village");
+        obj.borrowBook("My journey");
         int result = obj.availableBook();
         assertEquals(0, result);
     }
 
     @Test
     public void returnBookTest() {
-        obj.addBook("isbn111", "my name", "gautam chauhan", 2024);
-        obj.borrowBook("my name");
-        obj.returnBook("my name");
+        obj.addBook("isbn111", "my village", "gautam chauhan", 2024);
+        obj.borrowBook("my village");
+        obj.returnBook("my village");
         int result = obj.availableBook();
         assertEquals(1, result);
     }
 
     @Test
     public void returnBookNotBorrowedTest() {
-        obj.addBook("isbn111", "my name", "gautam chauhan", 2024);
-        obj.borrowBook("my name");
+        obj.addBook("isbn111", "my village", "gautam chauhan", 2024);
+        obj.borrowBook("my village");
         obj.returnBook("my journey");
         int result = obj.availableBook();
         assertEquals(0, result, "This book is not borrowed from this Library");
@@ -60,10 +60,10 @@ public class LibraryTest {
 
     @Test
     public void viewAvailableBoookTest() {
-        obj.addBook("isbn111", "my name", "gautam chauhan", 2024);
+        obj.addBook("isbn111", "my village", "gautam chauhan", 2024);
         obj.addBook("isbn106", "my journey", "gautam chauhan", 2024);
         assertEquals(2, obj.availableBook(), "Available books");
-        obj.borrowBook("my name");
+        obj.borrowBook("my village");
         assertEquals(1, obj.availableBook(), "Available books");
     }
 }
